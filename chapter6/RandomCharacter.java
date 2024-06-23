cee8b3b4c95827371972e193e3caa03be2c5eb81
utkapodsousom <utkapodsousom@gmail.com>
2024-06-23 13:39:55 +0300

    finished random chars listing

---
+++ chapter6/RandomCharacter.java
@@ -0,0 +1,26 @@
+package chapter6;
+
+public class RandomCharacter {
+    public static char getRandomCharacter(char ch1, char ch2) {
+        return (char) (ch1 + Math.random() * (ch2 - ch1 + 1));
+    }
+    
+    public static char getRandomLowerCaseLetter() {
+        return getRandomCharacter('a', 'z');
+    }
+
+    public static char getRandomUpperCaseLetter() {
+        return getRandomCharacter('A', 'Z');
+    }
+
+    public static int getRandomDigitCharacter() {
+        return getRandomCharacter('0', '9');
+    }
+
+    public static char getRandomCharacter() {
+        return getRandomCharacter('\u0000', '\uFFFF');
+    }
+
+    public static void main(String[] args) {
+    }
+}
