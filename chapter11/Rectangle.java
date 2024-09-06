8371daf68e99d428f831cbda72b5816576d8f802
utkapodsousom <utkapodsousom@gmail.com>
2024-09-06 16:02:31 +0300

    dynamic binding

---
+++ chapter11/Rectangle.java
@@ -0,0 +1,43 @@
+package chapter11;
+
+public class Rectangle extends GeometricObject {
+    private double width;
+    private double height;
+
+    public Rectangle() {}
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(double width, double height, String color, boolean filed) {
+        super(color, filed);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return (width + height) * 2;
+    }
+}
